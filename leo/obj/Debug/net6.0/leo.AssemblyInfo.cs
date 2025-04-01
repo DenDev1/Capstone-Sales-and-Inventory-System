d97baadbee0c8fc55ce27a5d18248a8ed4aa7a10
DenDev1 <dendopulgo123@gmail.com>
2025-04-01 17:33:32 +0800

    Done Fixed Orders

--- leo/obj/Debug/net6.0/leo.AssemblyInfo.cs
+++ leo/obj/Debug/net6.0/leo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4594b7e5e004fee197608fbdbcba40d6c5453c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08131cb437c303f632e20a24a5098badbf402cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("leo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
